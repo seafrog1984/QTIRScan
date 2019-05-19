09a876686a58408f5f7e0390f35e0a285dd117a9
seafrog1984 <seafrog1984@hotmail.com>
2019-05-19 22:28:45 +0800

    Modify UI for laptop

--- IRScan/IRScan/main.cpp
+++ IRScan/IRScan/main.cpp
@@ -1,9 +1,15 @@
 #include "IRScan.h"
 #include <QtWidgets/QApplication>
 
+
+
+
 int main(int argc, char *argv[])
-{
+{	
+
+	QApplication::addLibraryPath("./plugins");
 	QApplication a(argc, argv);
+
 	IRScan w;
 	w.show();
 	return a.exec();
